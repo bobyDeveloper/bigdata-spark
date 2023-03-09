6b16a97c16c74bbfd6a354458388a2493de7188f
bobyDeveloper <118419773+bobyDeveloper@users.noreply.github.com>
2023-03-09 23:40:33 +0000

    traza_de_matrices

---
+++ onlyMatlab/traza.m
@@ -0,0 +1,11 @@
+// Definir una matriz de ejemplo
+A = [1, 2, 3;
+     4, 5, 6;
+     7, 8, 9]
+
+// Calcular la traza de la matriz A
+traza_A = trace(A)
+
+// Imprimir el resultado
+println("La traza de A es:", traza_A)
+
